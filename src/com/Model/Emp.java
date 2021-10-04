90ec6634cb57d4ad6f44739dd6393bdd1083afcd
deepa <tqdeeepa@gmail.com>
2021-10-04 16:56:32 +0530

    First commit

---
+++ src/com/Model/Emp.java
@@ -0,0 +1,24 @@
+package com.Model;
+
+public class Emp {
+private int eid;
+private String ename;
+public int getEid() {
+	return eid;
+}
+public void setEid(int eid) {
+	this.eid = eid;
+}
+public String getEname() {
+	return ename;
+}
+public void setEname(String ename) {
+	this.ename = ename;
+}
+public Emp(int eid, String ename) {
+	super();
+	this.eid = eid;
+	this.ename = ename;
+}
+
+}
